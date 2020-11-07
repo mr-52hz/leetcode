e0c3a2883acd62897a9b99e06eb604a0e0ec8938
mr_52hz <1871253831@qq.com>
2020-11-07 16:32:49 +0800

    [refactor]冒泡排序优化-添加交换标识，判断冒泡提前结束点，节省剩余排好的扫描时间。

--- practice/MOOC/数据结构与算法/7-查找与排序/503冒泡和选择排序.py
+++ practice/MOOC/数据结构与算法/7-查找与排序/503冒泡和选择排序.py
@@ -19,8 +19,45 @@ def bubbleSort(unorder_list, reverse=False):
     return unorder_list
 
 
+"""
+冒泡排序的复杂度分析及优劣势
+    复杂度
+        对比 1~n-1 > (1+n-1)*(n-1)/2
+        交换 最差情况就是每次对比都交换 = 对比次数
+        所以 (1+n-1)*(n-1)/2*2 = O(n^2) 
+    优点
+        无需额外的存储空间的开销
+    缺点
+        必须要经过多次的对比和交换、其中大部分操作都不是最终的交换
+        
+    优化
+        可以设置标识，如果在一次对比中没有发生任何交换，则意味着列表已经有序，可以提前结束
+        虽然能够节省部分时间，但是不能改变冒泡排序的时间复杂度。
+"""
+
+
+def shortBubbleSort(unorder_list, reverse=False):
+    for scan_length in range(len(unorder_list)-1, 0, -1):
+        is_exchange = False
+        for i in range(scan_length):
+            if reverse is True:
+                if unorder_list[i] < unorder_list[i+1]:
+                    unorder_list[i], unorder_list[i+1] = unorder_list[i+1], unorder_list[i]
+                    is_exchange = True
+            else:
+                if unorder_list[i] > unorder_list[i+1]:
+                    unorder_list[i], unorder_list[i+1] = unorder_list[i+1], unorder_list[i]
+                    is_exchange = True
+
+        if is_exchange is False:
+            break
+    return unorder_list
+
+
 if __name__ == '__main__':
     u_ls = [1, 3, 2, 7, 4]
     ls = bubbleSort(u_ls, reverse=True)
     print(ls)
+    ls2 = shortBubbleSort(u_ls)
+    print(ls2)
 
