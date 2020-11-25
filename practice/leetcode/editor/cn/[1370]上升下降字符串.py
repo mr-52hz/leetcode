50cdf8e97c5c6c7d3eb299280efdf4609bd5035e
mr_52hz <1871253831@qq.com>
2020-11-25 18:28:58 +0800

    [feature]上升下降字符串-解答成功:执行耗时:188 ms,击败了17.37% 的Python用户,内存消耗:13.2 MB,击败了13.25% 的Python用户

---
+++ practice/leetcode/editor/cn/[1370]上升下降字符串.py
@@ -0,0 +1,115 @@
+# 给你一个字符串 s ，请你根据下面的算法重新构造字符串： 
+# 
+#  
+#  从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。 
+#  从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。 
+#  重复步骤 2 ，直到你没法从 s 中选择字符。 
+#  从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。 
+#  从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。 
+#  重复步骤 5 ，直到你没法从 s 中选择字符。 
+#  重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。 
+#  
+# 
+#  在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。 
+# 
+#  请你返回将 s 中字符重新排序后的 结果字符串 。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：s = "aaaabbbbcccc"
+# 输出："abccbaabccba"
+# 解释：第一轮的步骤 1，2，3 后，结果字符串为 result = "abc"
+# 第一轮的步骤 4，5，6 后，结果字符串为 result = "abccba"
+# 第一轮结束，现在 s = "aabbcc" ，我们再次回到步骤 1
+# 第二轮的步骤 1，2，3 后，结果字符串为 result = "abccbaabc"
+# 第二轮的步骤 4，5，6 后，结果字符串为 result = "abccbaabccba"
+#  
+# 
+#  示例 2： 
+# 
+#  输入：s = "rat"
+# 输出："art"
+# 解释：单词 "rat" 在上述算法重排序以后变成 "art"
+#  
+# 
+#  示例 3： 
+# 
+#  输入：s = "leetcode"
+# 输出："cdelotee"
+#  
+# 
+#  示例 4： 
+# 
+#  输入：s = "ggggggg"
+# 输出："ggggggg"
+#  
+# 
+#  示例 5： 
+# 
+#  输入：s = "spo"
+# 输出："ops"
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= s.length <= 500 
+#  s 只包含小写英文字母。 
+#  
+#  Related Topics 排序 字符串 
+#  👍 59 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def sortString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s_ls = sorted(list(s))
+        result_s = ''
+        while s_ls:
+            result_s += self.step_1(s_ls)
+            if not s_ls:
+                break
+            result_s += self.step_2(s_ls)
+
+        return result_s
+
+    @staticmethod
+    def step_1(s_ls):
+        step_1_s = s_ls.pop(0)
+        count = s_ls.count(step_1_s)
+        while len(s_ls) > count:
+            s_buffer = s_ls.pop(count)
+            count += s_ls.count(s_buffer)
+            step_1_s += s_buffer
+        return step_1_s
+
+    @staticmethod
+    def step_2(s_ls):
+        step_2_s = s_ls.pop(-1)
+        count = s_ls.count(step_2_s)
+        while len(s_ls) > count:
+            s_buffer = s_ls.pop(-(count+1))
+            count += s_ls.count(s_buffer)
+            step_2_s += s_buffer
+        return step_2_s
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    #  输入：s = "leetcode"
+    # 输出："cdelotee"
+
+    #  输入：s = "aaaabbbbcccc"
+    # 输出："abccbaabccba"
+    s = 'cdelotee'
+    solution = Solution()
+    print(solution.sortString(s))
